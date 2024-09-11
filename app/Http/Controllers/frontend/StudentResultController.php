be678be4169b5a72150176b54e0eb8e8840a76ab
abyankamal <abyankamal8@gmail.com>
2024-09-11 10:50:45 +0700

    thirty commit

--- app/Http/Controllers/frontend/StudentResultController.php
+++ app/Http/Controllers/frontend/StudentResultController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Classes;
+use App\Models\Result;
+use App\Models\Student;
 use Illuminate\Http\Request;
 
 class StudentResultController extends Controller
@@ -13,5 +15,32 @@ class StudentResultController extends Controller
     {
         $classes = Classes::all();
         return view('frontend.index', compact('classes'));
+    }
+
+    public function SearchResult(Request $request)
+    {
+        $roll_id = $request->roll_id;
+        $class_id = $request->class_id;
+        $student = Student::where('roll_id', $roll_id)->where('class_id', $class_id)->get();
+
+        if (!$student) {
+            $notification = array(
+                'message' => 'Student Not Found',
+                'alert-type' => 'error'
+            );
+            return redirect()->back()->with($notification);
+        }
+
+        $result = Result::where('student_id', $student[0]->id)->get();
+
+        if (count($result) == 0) {
+            $notification = array(
+                'message' => 'Sorry Result Not Declared Yet',
+                'alert-type' => 'error'
+            );
+            return redirect()->back()->with($notification);
+        }
+
+        return view('frontend.student_result', compact('result'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -10,72 +10,73 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [StudentResultController::class, 'index'])->name('index');
+Route::post('student/result', [StudentResultController::class, 'SearchResult'])->name('search.result');
 
 Route::get('/dashboard', function () {
     return view('admin.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::controller(AdminController::class)->group(function () {
-    Route::get('admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-    Route::get('admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-    Route::post('admin/profile/update', [AdminController::class, 'AdminProfileUpdate'])->name('admin.profile.update');
-    Route::get('admin/profile/change-password', [AdminController::class, 'AdminChangePassword'])->name('admin.change-password');
-    Route::post('admin/profile/change-password', [AdminController::class, 'AdminChangePasswordUpdate'])->name('admin.password.update');
-});
-
-Route::controller(ClassesController::class)->group(function () {
-    Route::get('create/class', [ClassesController::class, 'CreateClass'])->name('create.class');
-    Route::post('create/class', [ClassesController::class, 'StoreClass'])->name('store.class');
-    Route::get('manage/class', [ClassesController::class, 'ManageClass'])->name('manage.class');
-    Route::get('edit/class/{id}', [ClassesController::class, 'EditClass'])->name('edit.class');
-    Route::post('update/class', [ClassesController::class, 'UpdateClass'])->name('update.class');
-    Route::get('delete/class/{id}', [ClassesController::class, 'DeleteClass'])->name('delete.class');
-});
-
-Route::controller(SubjectController::class)->group(function () {
-    Route::get('create/subject', [SubjectController::class, 'CreateSubject'])->name('create.subject');
-    Route::post('create/subject', [SubjectController::class, 'StoreSubject'])->name('store.subject');
-    Route::get('manage/subject', [SubjectController::class, 'ManageSubject'])->name('manage.subject');
-    Route::get('edit/subject/{id}', [SubjectController::class, 'EditSubject'])->name('edit.subject');
-    Route::post('update/subject', [SubjectController::class, 'UpdateSubject'])->name('update.subject');
-    Route::get('delete/subject/{id}', [SubjectController::class, 'DeleteSubject'])->name('delete.subject');
-
-    // Subject Combination All Routes
-    Route::get('add/subject/combination', [SubjectController::class, 'AddSubjectCombination'])->name('add.subject.combination');
-    Route::post('add/subject/combination', [SubjectController::class, 'StoreSubjectCombination'])->name('store.subject.combination');
-    Route::get('manage/subject/combination', [SubjectController::class, 'ManageSubjectCombination'])->name('manage.subject.combination');
-    Route::get('deactivate/subject/combination/{id}', [SubjectController::class, 'DeactivateSubjectCombination'])->name('deactivate.subject.combination');
-});
-
-Route::controller(StudentController::class)->group(function () {
-    Route::get('add/student', [StudentController::class, 'AddStudent'])->name('add.student');
-    Route::post('add/student', [StudentController::class, 'StoreStudent'])->name('store.student');
-    Route::get('manage/student', [SubjectController::class, 'ManageStudent'])->name('manage.student');
-    Route::get('edit/student/{id}', [StudentController::class, 'EditStudent'])->name('edit.student');
-    Route::post('update/student', [StudentController::class, 'UpdateStudent'])->name('update.student');
-    Route::get('delete/student/{id}', [StudentController::class, 'DeleteStudent'])->name('delete.student');
-});
-
-Route::controller(ResultController::class)->group(function () {
-    Route::get('add/result', [ResultController::class, 'AddResult'])->name('add.result');
-    Route::post('add/result', [ResultController::class, 'StoreResult'])->name('store.result');
-    Route::get('manage/result', [ResultController::class, 'ManageResult'])->name('manage.result');
-    Route::get('edit/result/{id}', [ResultController::class, 'EditResult'])->name('edit.result');
-    Route::post('update/result', [ResultController::class, 'UpdateResult'])->name('update.result');
-    Route::get('delete/result/{id}', [ResultController::class, 'DeleteResult'])->name('delete.result');
-
-
-
-    // fetch the result
-    Route::get('fetch/student', [ResultController::class, 'FetchStudent'])->name('fetch.student');
-    Route::get('fetch/student/result', [ResultController::class, 'FetchStudentResult'])->name('check.student.result');
-});
 
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::controller(AdminController::class)->group(function () {
+        Route::get('admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+        Route::get('admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+        Route::post('admin/profile/update', [AdminController::class, 'AdminProfileUpdate'])->name('admin.profile.update');
+        Route::get('admin/profile/change-password', [AdminController::class, 'AdminChangePassword'])->name('admin.change-password');
+        Route::post('admin/profile/change-password', [AdminController::class, 'AdminChangePasswordUpdate'])->name('admin.password.update');
+    });
+
+    Route::controller(ClassesController::class)->group(function () {
+        Route::get('create/class', [ClassesController::class, 'CreateClass'])->name('create.class');
+        Route::post('create/class', [ClassesController::class, 'StoreClass'])->name('store.class');
+        Route::get('manage/class', [ClassesController::class, 'ManageClass'])->name('manage.class');
+        Route::get('edit/class/{id}', [ClassesController::class, 'EditClass'])->name('edit.class');
+        Route::post('update/class', [ClassesController::class, 'UpdateClass'])->name('update.class');
+        Route::get('delete/class/{id}', [ClassesController::class, 'DeleteClass'])->name('delete.class');
+    });
+
+    Route::controller(SubjectController::class)->group(function () {
+        Route::get('create/subject', [SubjectController::class, 'CreateSubject'])->name('create.subject');
+        Route::post('create/subject', [SubjectController::class, 'StoreSubject'])->name('store.subject');
+        Route::get('manage/subject', [SubjectController::class, 'ManageSubject'])->name('manage.subject');
+        Route::get('edit/subject/{id}', [SubjectController::class, 'EditSubject'])->name('edit.subject');
+        Route::post('update/subject', [SubjectController::class, 'UpdateSubject'])->name('update.subject');
+        Route::get('delete/subject/{id}', [SubjectController::class, 'DeleteSubject'])->name('delete.subject');
+
+        // Subject Combination All Routes
+        Route::get('add/subject/combination', [SubjectController::class, 'AddSubjectCombination'])->name('add.subject.combination');
+        Route::post('add/subject/combination', [SubjectController::class, 'StoreSubjectCombination'])->name('store.subject.combination');
+        Route::get('manage/subject/combination', [SubjectController::class, 'ManageSubjectCombination'])->name('manage.subject.combination');
+        Route::get('deactivate/subject/combination/{id}', [SubjectController::class, 'DeactivateSubjectCombination'])->name('deactivate.subject.combination');
+    });
+
+    Route::controller(StudentController::class)->group(function () {
+        Route::get('add/student', [StudentController::class, 'AddStudent'])->name('add.student');
+        Route::post('add/student', [StudentController::class, 'StoreStudent'])->name('store.student');
+        Route::get('manage/student', [SubjectController::class, 'ManageStudent'])->name('manage.student');
+        Route::get('edit/student/{id}', [StudentController::class, 'EditStudent'])->name('edit.student');
+        Route::post('update/student', [StudentController::class, 'UpdateStudent'])->name('update.student');
+        Route::get('delete/student/{id}', [StudentController::class, 'DeleteStudent'])->name('delete.student');
+    });
+
+    Route::controller(ResultController::class)->group(function () {
+        Route::get('add/result', [ResultController::class, 'AddResult'])->name('add.result');
+        Route::post('add/result', [ResultController::class, 'StoreResult'])->name('store.result');
+        Route::get('manage/result', [ResultController::class, 'ManageResult'])->name('manage.result');
+        Route::get('edit/result/{id}', [ResultController::class, 'EditResult'])->name('edit.result');
+        Route::post('update/result', [ResultController::class, 'UpdateResult'])->name('update.result');
+        Route::get('delete/result/{id}', [ResultController::class, 'DeleteResult'])->name('delete.result');
+
+
+
+        // fetch the result
+        Route::get('fetch/student', [ResultController::class, 'FetchStudent'])->name('fetch.student');
+        Route::get('fetch/student/result', [ResultController::class, 'FetchStudentResult'])->name('check.student.result');
+    });
 });
 
 require __DIR__ . '/auth.php';
